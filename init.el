c11f506d64caa68988976f994e04a93e51302fde
Logan Linn <logan@loganlinn.com>
2023-10-08 16:37:57 -0700

    nix +lsp

--- init.el
+++ init.el
@@ -159,7 +159,7 @@
        (lua +lsp +fennel)             ; one-based indices? one-based indices
        (markdown +grip)               ; writing docs for people to ignore
        ;;nim                          ; python + lisp at the speed of c
-       (nix +tree-sitter)             ; I hereby declare "nix geht mehr!"
+       (nix +lsp +tree-sitter)             ; I hereby declare "nix geht mehr!"
        ;;ocaml                        ; an objective camel
        (org
         +dragndrop
